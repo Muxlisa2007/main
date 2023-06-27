b57f8244e9a3127394f788492515cbe934f77634
Muxlisa2007 <muxlisa200707@gmail.com>
2023-06-27 08:45:34 +0500

    yuklandi

---
+++ mainn.py
@@ -0,0 +1,25 @@
+print('Asslomu alaykum')
+
+print('Hayrli tong')
+
+print(3*3)
+
+print(19/5)
+
+print(2**4)
+
+print('OUTPUT: "Nexia","Tico","Damas"ko`rganlar qilar havas')
+
+Bool_1 = True
+Bool_1 = False
+print(Bool_1)
+
+string = "Hello world"
+print(string)
+
+sonli = 4596
+print(sonli)
+
+mantiqiy = True
+print(mantiqiy)
+
